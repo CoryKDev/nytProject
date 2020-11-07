1270b036ea03f496bc9db0d7342e855fb145f31c
Tskading <Tskading@gmail.com>
2020-11-07 14:06:15 -0800

    pushing incomplete js for all

--- nytProject/Instructions/script.js
+++ nytProject/Instructions/script.js
@@ -1,5 +1,12 @@
-$("search").on("submit", function(){
-    var searchEL = $(this).attr("searchEl");
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchEL + "&api-key=khDvOG8iazyG2Ykbx5sJ7MD9ZCdPACjY"
+$("input").on("click", function(){
+    var searchEL = $(this).attr("id");
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchEL + "&api-key=khDvOG8iazyG2Ykbx5sJ7MD9ZCdPACjY";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    })
 
 })
